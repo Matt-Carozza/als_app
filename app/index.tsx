dca5fb8309e129d3e9fb61aa52efed5e65b3112e
Matt Carozza <mcar728@gmail.com>
2025-06-02 18:11:05 -0400

    Add color and light mode dropdown

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { Picker } from '@react-native-picker/picker';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
 
-export default function Index() {
+export default function App() {
+  const [selectedLightMode, setSelectedLightMode] = useState();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Select Light Mode:</Text>
+      <Picker
+        selectedValue={selectedLightMode}
+        onValueChange={(itemValue, itemIndex) => setSelectedLightMode(itemValue)}
+      >
+        <Picker.Item label="White Light" value="cl" />
+        <Picker.Item label="Colored Light" value="wl" />
+      </Picker>
     </View>
   );
 }
