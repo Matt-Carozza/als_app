f99c49e1625156f73a95cde4421601d9d4e7e6f4
Matt Carozza <mcar728@gmail.com>
2025-06-03 20:50:33 -0400

    Add color picker to app

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,11 @@ import { globalStyles } from '@/styles/globalStyles';
 import { Picker } from '@react-native-picker/picker';
 import React, { useState } from 'react';
 import { Text, View } from 'react-native';
+import ColorPicker from 'react-native-wheel-color-picker';
 
 export default function App() {
   const [selectedLightMode, setSelectedLightMode] = useState();
+  const [selectedColor, setSelectedColor] = useState('#ffffff');
 
   return (
     <View style={globalStyles.screenContainer} >
@@ -14,9 +16,18 @@ export default function App() {
         onValueChange={(itemValue, itemIndex) => setSelectedLightMode(itemValue)}
         style={globalStyles.picker}
       >
-        <Picker.Item label="White Light" value="cl" />
-        <Picker.Item label="Colored Light" value="wl" />
+        <Picker.Item label="White Light" value="wl" />
+        <Picker.Item label="Colored Light" value="cl" />
       </Picker>
+      {selectedLightMode === 'cl' && (
+        <ColorPicker 
+          color={selectedColor}
+          onColorChangeComplete={color => setSelectedColor(color)}
+          thumbSize={40}
+          sliderSize={40}
+          noSnap={true}
+        />
+      )}
     </View>
   );
 }
