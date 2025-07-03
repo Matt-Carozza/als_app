23ff6bd349bdc9cd95cf11b7accbf45c1c505478
Matt Carozza <mcar728@gmail.com>
2025-07-03 15:58:14 -0400

    Add adaptive lighting mode

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -14,6 +14,8 @@ export const Colors = {
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+    secondary: '#7E2533',
+    secondaryLight: '#B6757D',
   },
   dark: {
     text: '#ECEDEE',
@@ -22,5 +24,7 @@ export const Colors = {
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+    secondary: '#7E2533',
+    secondaryLight: '#B6757D',
   },
 };
